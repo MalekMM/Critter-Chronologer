01e76ae5dab8a051ca08bd3e42a4b0ac3364f0e6
Malek M'hiri <malek.mhiri@tum.de>
2021-05-27 13:28:45 +0200

    schedule service and repository

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
@@ -0,0 +1,65 @@
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.model.Customer;
+import com.udacity.jdnd.course3.critter.model.Employee;
+import com.udacity.jdnd.course3.critter.model.Pet;
+import com.udacity.jdnd.course3.critter.model.Schedule;
+import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
+import com.udacity.jdnd.course3.critter.repository.EmployeeRepository;
+import com.udacity.jdnd.course3.critter.repository.PetRepository;
+import com.udacity.jdnd.course3.critter.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+    private final CustomerRepository customerRepository;
+    private final PetRepository petRepository;
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+
+    public ScheduleService(ScheduleRepository scheduleRepository, CustomerRepository customerRepository,
+                           PetRepository petRepository, EmployeeRepository employeeRepository) {
+        this.scheduleRepository = scheduleRepository;
+        this.customerRepository = customerRepository;
+        this.petRepository = petRepository;
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Schedule save(Schedule schedule, List<Long> employeeIds,
+                         List<Long> petIds) {
+        // get employees and pets and add them to the schedule
+        List<Employee> employees = employeeRepository.findAllById(employeeIds);
+        List<Pet> pets = petRepository.findAllById(petIds);
+
+        schedule.setEmployees(employees);
+        schedule.setPets(pets);
+        // save and return
+        return scheduleRepository.save(schedule);
+    }
+
+    public List<Schedule> getAllSchedules(){
+        return scheduleRepository.findAll();
+    }
+
+    public List<Schedule> getScheduleForPet(Long petId){
+        Pet pet = petRepository.getOne(petId);
+        return scheduleRepository.findByPets(pet);
+    }
+
+    public List<Schedule> getScheduleForEmployee(Long employeeId){
+        Employee employee = employeeRepository.getOne(employeeId);
+        return scheduleRepository.findByEmployees(employee);
+    }
+
+    public List<Schedule> getScheduleForCustomer(Long customerId){
+        Customer customer = customerRepository.getOne(customerId);
+        return scheduleRepository.findAllByPetsIn(customer.getPets());
+    }
+
+}
