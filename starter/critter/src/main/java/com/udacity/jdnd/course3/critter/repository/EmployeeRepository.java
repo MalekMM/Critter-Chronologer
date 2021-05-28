fa3e4c4f10cb671a022568c978f697d367a76714
Malek M'hiri <malek.mhiri@tum.de>
2021-05-28 08:53:03 +0200

    added @Transactional annotation

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
@@ -4,10 +4,12 @@ import com.udacity.jdnd.course3.critter.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.time.DayOfWeek;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     List<Employee> findAllByDaysAvailable(DayOfWeek day);
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -6,9 +6,11 @@ import com.udacity.jdnd.course3.critter.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> findByPets(Pet pet);
